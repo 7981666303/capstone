d7e6b8664a9608fd64dcadb2b836f52765da3db4
7981666303 <bandarukaliprasad123@gmail.com>
2026-04-27 02:04:18 +0530

    updated login.jsx

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,14 @@
-import axios from 'axios';
+// import axios from 'axios';
+
+// const api = axios.create({
+//     baseURL: import.meta.env.VITE_API_URL || "https://capstone-3hbm.onrender.com", // Dynamic Server URL
+// });
+
+// export default api;
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "https://capstone-3hbm.onrender.com", // Dynamic Server URL
+  baseURL: "https://capstone-3hbm.onrender.com",
 });
 
 export default api;
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -18,17 +18,19 @@ const Login = () => {
         setIsLoading(true);
         setError('');
         try {
-            const response = await fetch('http://localhost:5000/api/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    username: formData.username,
-                    password: formData.password,
-                    role: role
-                }),
-            });
+            const response = await fetch(
+                `${import.meta.env.VITE_API_URL}/api/login`,
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        username: formData.username,
+                        password: formData.password,
+                        role: role
+                    }),
+                });
 
             const data = await response.json();
 
