bc09a32027191ff49b66a5211eaa6355cec798f3
7981666303 <bandarukaliprasad123@gmail.com>
2026-04-29 23:02:22 +0530

    chore: remove gradle cache files from git tracking

--- server/index.js
+++ server/index.js
@@ -43,7 +43,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
 
     // For any other requests, serve the React app's index.html
-    app.get('*', (req, res) => {
+    app.get('*splat', (req, res) => {
         res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
     });
 }
