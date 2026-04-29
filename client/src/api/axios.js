a02e5481971309468496052cd6f1c5aa7dac7929
prasad <119881079+7981666303@users.noreply.github.com>
2026-04-29 22:32:28 +0530

    Update axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "https://capstone-3hbm.onrender.com", // Dynamic Server URL
+    baseURL: import.meta.env.VITE_API_URL || "https://capstone-3hbm.onrender.com/api", // Dynamic Server URL
 });
 
 export default api;
